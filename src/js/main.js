bc91959cbbcf3d7ab37426cc3c6b228ed4f81d5c
witnip <70694885+witnip@users.noreply.github.com>
2024-12-04 10:27:18 +0530

    done

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,202 @@
-class Game{
-    constructor(canvas,context){
+class Game {
+    constructor(canvas, context) {
         this.canvas = canvas;
         this.ctx = context;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.baseHeight = 720;
         this.ratio = this.height / this.baseHeight;
+        this.background = new Background(this);
+        this.sound = new AudioControl();
         this.player = new Player(this);
+        this.obstacles = [];
+        this.numberOfObstacles = 2;
+        this.eventTimer = 0;
+        this.eventInterval = 150;
+        this.eventUpdate = false;
+        this.swipeDistance = 50;
+        this.debug = false;
 
         this.resize(window.innerWidth, window.innerHeight);
 
-        window.addEventListener('resize',e => {
+        window.addEventListener('resize', e => {
             this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight);
         });
 
         window.addEventListener('mousedown', e => {
             this.player.flap();
         })
+
+        window.addEventListener('mouseup', e => {
+            setTimeout(() =>{
+                this.player.wingsUp();
+            },50);
+        })
+
+        window.addEventListener('keydown', e => {
+            if (e.key === ' ' || e.key === 'Enter') this.player.flap();
+            if (e.key === 'Shift' || e.key.toLowerCase() === 'c') this.player.startCharge();
+            if (e.key.toLowerCase() === 'r') {
+                this.resize(window.innerWidth, window.innerHeight);
+                this.numberOfObstacles++;
+            }
+            if (e.key.toLowerCase() === 'd') this.debug = !this.debug;
+        });
+
+        // window.addEventListener('keyup', e => {
+        //     if (e.key === 'Shift' || e.key.toLowerCase() === 'c') this.player.stopCharge();
+        // })
+
+
+        window.addEventListener('touchstart', e => {
+            this.player.flap();
+            this.touchStartX = e.changedTouches[0].pageX;
+        });
+
+        window.addEventListener('touchmove', e => {
+            e.preventDefault();
+        });
+
+        window.addEventListener('touchend', e => {
+            if (e.changedTouches[0].pageX - this.touchStartX > this.swipeDistance) {
+                this.player.startCharge();
+            }else {
+                this.player.flap();
+            }
+        })
     }
 
     resize(width, height) {
         this.canvas.width = width;
         this.canvas.height = height;
-        this.ctx.fillStyle = 'red';
+        this.ctx.textAlign = 'right';
+        this.ctx.lineWidth = 1;
+        this.ctx.strokeStyle = 'white';
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.ratio = this.height / this.baseHeight;
 
+        this.bottomMargin = Math.floor(50 * this.ratio);
+        this.smallFont = Math.ceil(20 * this.ratio);
+        this.largeFont = Math.ceil(45 * this.ratio);
+        this.ctx.font = this.smallFont + 'px Bungee';
         this.gravity = 0.15 * this.ratio;
+        this.speed = 2 * this.ratio;
+        this.minSpeed = this.speed;
+        this.maxSpeed = this.speed * 5;
+        this.background.resize();
         this.player.resize();
+        this.createObstacles();
+        this.obstacles.forEach(obstacle => {
+            obstacle.resize();
+        });
+        this.score = 0;
+        this.gameOver = false;
+        this.timer = 0;
     }
 
-    render(){
+    render(deltaTime) {
+        if (!this.gameOver) this.timer += deltaTime;
+        this.handlePeriodicEvent(deltaTime);
+        this.background.update();
+        this.background.draw();
+        this.drawStatusText();
         this.player.update();
         this.player.draw();
+        this.obstacles.forEach(obstacle => {
+            obstacle.update();
+            obstacle.draw();
+        });
+    }
+
+    createObstacles() {
+        this.obstacles = [];
+        const fistX = this.baseHeight * this.ratio;
+        const obstaclesSpacing = 600 * this.ratio;
+        for (let i = 0; i < this.numberOfObstacles; i++) {
+            this.obstacles.push(new Obstacle(this, fistX + i * obstaclesSpacing));
+        }
+    }
+
+    checkCollision(a, b) {
+        const dx = a.collisionX - b.collisionX;
+        const dy = a.collisionY - b.collisionY;
+        const distance = Math.hypot(dx, dy);
+        const sumOfRadii = a.collisionRadius + b.collisionRadius;
+        return distance <= sumOfRadii;
+    }
+
+    handlePeriodicEvent(deltaTime) {
+        if (this.eventTimer < this.eventInterval) {
+            this.eventTimer += deltaTime;
+            this.eventUpdate = false;
+        } else {
+            this.eventTimer = this.eventTimer % this.eventInterval;
+            this.eventUpdate = true;
+        }
+    }
+
+    formatTimer() {
+        return (this.timer * 0.001).toFixed(1);
+    }
+
+    triggerGameOver() {
+        if (!this.gameOver) {
+            this.gameOver = true;
+            if (this.obstacles.length <= 0) {
+                this.sound.play(this.sound.win);
+                this.message1 = "Nailed it!";
+                this.message2 = "Can you do it faster than " + this.formatTimer() + " seconds!";
+            } else {
+                this.numberOfObstacles = 2;
+                this.sound.play(this.sound.lose);
+                this.message1 = "Getting rusty?";
+                this.message2 = "collision time " + this.formatTimer() + " seconds!";
+            }
+        }
+    }
+
+    drawStatusText() {
+        this.ctx.save();
+        this.ctx.fillText('Score: ' + this.score, this.width - this.smallFont, this.largeFont);
+        this.ctx.textAlign = 'left';
+        this.ctx.fillText('Timer: ' + this.formatTimer(), this.smallFont, this.largeFont);
+        if (this.gameOver) {
+            this.ctx.textAlign = 'center';
+            this.ctx.font = this.largeFont + 'px Bungee';
+            this.ctx.fillText(this.message1, this.width * 0.5, this.height * 0.5 - this.largeFont, this.width);
+            this.ctx.font = this.smallFont + 'px Bungee';
+            this.ctx.fillText(this.message2, this.width * 0.5, this.height * 0.5 - this.smallFont, this.width);
+            this.ctx.fillText("Press 'R' to try again", this.width * 0.5, this.height * 0.5);
+        }
+        if (this.player.energy <= this.player.minEnergy) {
+            this.ctx.fillStyle = 'red';
+        } else if (this.player.energy >= this.player.maxEnergy) {
+            this.ctx.fillStyle = 'green';
+        }
+        for (let i = 0; i < this.player.energy; i++) {
+            this.ctx.fillRect(10, this.height - 10 - this.player.barsize * i, this.player.barsize * 5, this.player.barsize)
+        }
+        this.ctx.restore();
     }
 }
 
-window.addEventListener('load',function(){
-   const canvas = document.getElementById('canvas1');
-   const ctx = canvas.getContext('2d');
-   canvas.width = 720;
-   canvas.height = 720;
+window.addEventListener('load', function () {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 720;
+    canvas.height = 720;
 
-   const game = new Game(canvas,ctx);
+    const game = new Game(canvas, ctx);
 
-   function animate(){
-       ctx.clearRect(0, 0, canvas.width, canvas.height);
-       game.render();
-       requestAnimationFrame(animate);
-   }
-   requestAnimationFrame(animate);
+    let lastTime = 0;
+
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        game.render(deltaTime);
+        requestAnimationFrame(animate);
+    }
+
+    requestAnimationFrame(animate);
 });
--- src/js/player.js
+++ src/js/player.js
@@ -7,38 +7,113 @@ class Player {
         this.spriteHeight = 200;
         this.speedY = 0;
         this.flapSpeed = 0;
+        this.energy = 30;
+        this.maxEnergy = this.energy * 2;
+        this.minEnergy = 15;
+        this.image = document.getElementById("player_fish");
     }
 
-    draw(){
-        this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
+    draw() {
+        this.game.ctx.drawImage(this.image, 0, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        if(this.game.debug){
+            this.game.ctx.beginPath();
+            this.game.ctx.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+            this.game.ctx.stroke();
+        }
     }
-    update(){
+
+    update() {
+        this.handleEnergy();
+        if(this.speedY >= 0) this.wingsUp();
         this.y += this.speedY;
-        if(!this.isTouchingBottom()){
+        this.collisionY = this.y + this.height * 0.5;
+        if (!this.isTouchingBottom() && !this.charhing) {
             this.speedY += this.game.gravity;
+        } else {
+            this.speedY = 0;
         }
 
-        if(this.isTouchingBottom()){
-            this.y = this.game.height - this.height;
+        if (this.isTouchingBottom()) {
+            this.y = this.game.height - this.height - this.game.bottomMargin;
+            this.wingsIdle();
         }
     }
-    resize(){
+
+    resize() {
         this.width = this.spriteWidth * this.game.ratio;
         this.height = this.spriteHeight * this.game.ratio;
         this.y = this.game.height * 0.5 - this.height * 0.5;
         this.speedY = -8 * this.game.ratio;
         this.flapSpeed = 5 * this.game.ratio;
+        this.collisionRadius = 40 * this.game.ratio;
+        this.collisionX = this.x + this.collisionRadius * 0.9;
+        this.collided = false;
+        this.barsize = Math.floor(5 * this.game.ratio);
+        this.frameY = 0;
+        this.charhing = false;
     }
-    isTouchingTop(){
+
+    startCharge() {
+        if(this.energy >= this.minEnergy && !this.charhing){
+            this.charhing = true;
+            this.game.speed = this.game.maxSpeed;
+            this.wingsCharge();
+            this.game.sound.play(this.game.sound.charge);
+        }
+    }
+
+    stopCharge() {
+        this.charhing = false;
+        this.game.speed = this.game.minSpeed;
+        this.wingsIdle();
+    }
+
+    wingsIdle(){
+        if(!this.charhing) this.frameY = 0;
+    }
+
+    wingsDown(){
+        if(!this.charhing) this.frameY = 1;
+    }
+
+    wingsUp(){
+        if(!this.charhing) this.frameY = 2;
+    }
+
+    wingsCharge(){
+        this.frameY = 3;
+    }
+
+    isTouchingTop() {
         return this.y <= 0;
     }
 
-    isTouchingBottom(){
-        return this.y >= this.game.height - this.height;
+    isTouchingBottom() {
+        return this.y >= this.game.height - this.height - this.game.bottomMargin;
     }
-    flap(){
-        if(!this.isTouchingTop()){
+
+    handleEnergy() {
+        if (this.game.eventUpdate) {
+            if (this.energy < this.maxEnergy) {
+                this.energy += 1;
+            }
+
+            if (this.charhing) {
+                this.energy -= 4;
+                if (this.energy <= 0) {
+                    this.energy = 0;
+                    this.stopCharge();
+                }
+            }
+        }
+    }
+
+    flap() {
+        this.stopCharge();
+        this.game.sound.play(this.game.sound.flapSounds[Math.floor(Math.random() * 5)]);
+        if (!this.isTouchingTop()) {
             this.speedY = -this.flapSpeed;
+            this.wingsDown();
         }
     }
 }
